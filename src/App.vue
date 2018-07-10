7f179cf2cc6cf5918ab59158cca1403553067b19
Vinicius jorge <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-07-10 14:00:31 -0300

    Adicionar dropdown de teste

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
+    <br>
+    <dropdown class="ui container"></dropdown>
     <hello></hello>
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello.vue'
+import Dropdown from './components/Dropdown.vue'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Hello,
+    Dropdown
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 // This is done with the browser options. For the config, see package.json
 import Vue from 'vue'
 import App from './App.vue'
+import SuiVue from 'semantic-ui-vue'
+
+Vue.use(SuiVue);
 
 new Vue({ // eslint-disable-line no-new
   el: '#app',
   render: (h) => h(App)
 })
+
