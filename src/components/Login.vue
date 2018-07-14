156bf889895ef231b68406fcf18ee1621c1693c5
Vinícius Jorge Pereira <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-07-14 13:56:44 -0300

    alterar login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,50 +1,54 @@
 <template>
-	<div id="login">
-		<br>
-		<h2 class="ui icon center aligned header">
-			<i class="user icon"></i>
-			<div class="content">Log in</div>
-		</h2>
-		<form class="ui form warning">
-			<div class="field">
-				<label>Matricula</label>
-				<div class="ui left icon input">
-					<input v-model="matriculation" type="text" placeholder="Digite sua matricula" id="matriculation">
-					<i class="users icon"></i>
+	<div id="login" class="ui middle aligned center aligned grid">
+		<div class="column" style="max-width: 450px">
+			<br><br><br><br>
+			<h2 class="ui grey image header">
+				<i class="user icon"></i>
+				<div class="content">
+					Entre com a sua conta
 				</div>
-				<div class="ui warning message" v-if="matriculation.length < 9">
-    			<div class="header">Matricula inválida</div>
-  			</div>
-			</div>
-			<div class="field">
-				<label>Senha</label>
-				<div class="ui left icon input">
-					<input v-model="password" type="password" placeholder="Digite sua senha" id="password">
-					<i class="lock icon"></i>
-				</div>
-				<div class="ui warning message" v-if="password.length == 0">
-    			<div class="header">Senha inválida</div>
-  			</div>
-			</div>
-			<br>
-			<div class="grouped fields">
-				<label>Tipo de usuário:</label>
-				<div class="field">
-					<div class="ui radio checkbox">
-						<input type="radio" name="aluno">
-						<label>Aluno</label>
+				<br>
+			</h2>
+			<form class="ui larg form">
+				<div class="ui stacked blue segment">
+					<div class="field">
+						<div class="ui labeled icon input">
+							<div class="ui label">Matricula</div>
+							<input v-model="matriculation" type="text" placeholder="Digite sua matricula" id="matriculation">
+							<i class="user icon"></i>
+						</div>
 					</div>
-					<div class="ui radio checkbox">
-						<input type="radio" name="administrador">
-						<label>Administrador</label>
+					<div class="field">
+						<div class="ui labeled icon input">
+							<div class="ui label">Senha</div>
+							<input v-model="password" type="password" placeholder="Digite sua senha" id="password">
+							<i class="lock icon"></i>
+						</div>
 					</div>
+					<div class="grouped fields">
+						<label>Tipo de usuário:</label>
+						<div class="field">
+							<div class="ui radio checkbox">
+								<input type="radio" name="aluno">
+								<label>Aluno</label>
+							</div>
+							<div class="ui radio checkbox">
+								<input type="radio" name="administrador">
+								<label>Administrador</label>
+							</div>
+						</div>
+					</div>
+					<br>
+					<button class="ui fluid blue button" @click="setMatriculation(matriculation.value); setPassword(password.value)">
+						Entrar
+					</button>
 				</div>
+			</form>
+			<div class="ui grey message">
+				Novo por aqui?
+				<a href="#">Cadastre-se</a>
 			</div>
-			<br>
-			<button class="ui blue button" @click="setMatriculation(matriculation.value); setPassword(password.value)">
-				Entrar
-			</button>
-		</form>
+		</div>
 	</div>
 </template>
 
