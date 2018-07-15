9034a91aa583e260608a88d289b8bee1cad660ba
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-07-15 00:38:16 -0300

    remover Hello component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app" class="ui container">
-    <hello></hello>
     <login></login>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello.vue'
 import Dropdown from './components/Dropdown.vue'
 import Login from './components/Login.vue';
 
 export default {
   name: 'app',
   components: {
-    Hello,
     Dropdown,
     Login
   }
