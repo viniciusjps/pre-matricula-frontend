309bb421e5cefcb65189675d537934ce79a5716a
geovanens <geovane.silva@ccc.ufcg.edu.br>
2018-07-17 15:50:10 -0300

    adicionar dependecia para botao google e mudar componente login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,53 +1,8 @@
 <template>
 	<div id="login" class="ui middle aligned center aligned grid">
 		<div class="column" style="max-width: 450px">
-			<br><br><br><br>
-			<h2 class="ui grey image header">
-				<i class="user icon"></i>
-				<div class="content">
-					Entre com a sua conta
-				</div>
-				<br>
-			</h2>
-			<form class="ui larg form">
-				<div class="ui stacked blue segment">
-					<div class="field">
-						<div class="ui labeled icon input">
-							<div class="ui label">Matricula</div>
-							<input v-model="matriculation" type="text" placeholder="Digite sua matricula" id="matriculation">
-							<i class="user icon"></i>
-						</div>
-					</div>
-					<div class="field">
-						<div class="ui labeled icon input">
-							<div class="ui label">Senha</div>
-							<input v-model="password" type="password" placeholder="Digite sua senha" id="password">
-							<i class="lock icon"></i>
-						</div>
-					</div>
-					<div class="grouped fields">
-						<label>Tipo de usuário:</label>
-						<div class="field">
-							<div class="ui radio checkbox">
-								<input type="radio" name="aluno">
-								<label>Aluno</label>
-							</div>
-							<div class="ui radio checkbox">
-								<input type="radio" name="administrador">
-								<label>Administrador</label>
-							</div>
-						</div>
-					</div>
-					<br>
-					<button class="ui fluid blue button" @click="setMatriculation(matriculation.value); setPassword(password.value)">
-						Entrar
-					</button>
-				</div>
-			</form>
-			<div class="ui grey message">
-				Novo por aqui?
-				<a href="#">Cadastre-se</a>
-			</div>
+			<br><br><br><br><br><br><br><br><br><br>
+			<div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark" data-width="370" data-height="50" data-longtitle="true" data-lang="pt-BR"></div>
 		</div>
 	</div>
 </template>
