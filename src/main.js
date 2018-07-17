979b89ba6a0aa9f562ed1203384976947678af38
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-07-17 14:25:44 -0300

    add navbar

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import SuiVue from 'semantic-ui-vue'
+import router from './router'
 
 Vue.use(SuiVue);
 
 new Vue({ // eslint-disable-line no-new
+  router,
   el: '#app',
   render: (h) => h(App)
 })
