590853e29ef5bc2664478ad8622910f09d6f1634
hemillainysantos <hemillainy.santos@ccc.ufcg.edu.br>
2018-07-17 14:51:40 -0300

    add navbar em home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,36 +1,17 @@
 <template>
     <div id="home">
-        <div>
-            <div class="ui secondary pointing menu">
-                <a class="active item">
-                    <i class="home icon"></i> Home
-                </a>
-                <a class="item">
-                    <i class="mail icon"></i> Messages
-                </a>
-                <a class="item">
-                    <i class="user icon"></i> Friends
-                </a>
-                <div class="right menu">
-                    <div class="item">
-                        <div class="ui transparent icon input">
-                            <input type="text" placeholder="Search...">
-                            <i class="search link icon"></i>
-                        </div>
-                    </div>
-                    <a class="ui item">
-                    Logout
-                    </a>
-            </div>
-            </div>
-        </div>
+        <navbar></navbar>
     </div>
 </template>
 
 <script>
+import Navbar from './Navbar.vue'
 export default {
     name: 'home',
-    date(){
+    components: {
+        'navbar': Navbar
+        },
+    data(){
         return {
 
         }
---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="navbar">
+        <div>
+            <div class="ui secondary pointing menu">
+                <a class="active item">
+                    <i class="home icon"></i> Home
+                </a>
+                <a class="item">
+                    <i class="mail icon"></i> Messages
+                </a>
+                <a class="item">
+                    <i class="user icon"></i> Friends
+                </a>
+                <div class="right menu">
+                    <div class="item">
+                        <div class="ui transparent icon input">
+                            <input type="text" placeholder="Search...">
+                            <i class="search link icon"></i>
+                        </div>
+                    </div>
+                    <a class="ui item">
+                    Logout
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'navbar',
+    data() {
+        return {
+
+        }
+    }
+}    
+</script>
+
+<style scoped>
+    
+</style>
