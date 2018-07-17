4a020f3c9cb0500d22958cf4505fcdfadd0f53b5
Vinicius jorge <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-07-17 15:21:53 -0300

    alterando página de login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="login" class="ui middle aligned center aligned grid">
 		<div class="column" style="max-width: 450px">
-			<br><br><br><br>
+			<br><br><br><br><br><br><br><br>
 			<h2 class="ui grey image header">
 				<i class="user icon"></i>
 				<div class="content">
@@ -10,7 +10,7 @@
 				<br>
 			</h2>
 			<form class="ui larg form">
-				<div class="ui stacked blue segment">
+				<div class="ui stacked segment">
 					<div class="field">
 						<div class="ui labeled icon input">
 							<div class="ui label">Matricula</div>
@@ -25,29 +25,12 @@
 							<i class="lock icon"></i>
 						</div>
 					</div>
-					<div class="grouped fields">
-						<label>Tipo de usuário:</label>
-						<div class="field">
-							<div class="ui radio checkbox">
-								<input type="radio" name="aluno">
-								<label>Aluno</label>
-							</div>
-							<div class="ui radio checkbox">
-								<input type="radio" name="administrador">
-								<label>Administrador</label>
-							</div>
-						</div>
-					</div>
 					<br>
-					<button class="ui fluid blue button" @click="setMatriculation(matriculation.value); setPassword(password.value)">
+					<button class="ui fluid button" @click="setMatriculation(matriculation.value); setPassword(password.value)">
 						Entrar
 					</button>
 				</div>
 			</form>
-			<div class="ui grey message">
-				Novo por aqui?
-				<a href="#">Cadastre-se</a>
-			</div>
 		</div>
 	</div>
 </template>
@@ -77,4 +60,12 @@ export default {
 </script>
 
 <style scoped>
+.ui.fluid.button {
+	color: #FFFFFF;
+	background-color: #74accb
+}
+
+.ui.stacked.segment {
+	border-top: 2px solid #74accb!important
+}
 </style>
