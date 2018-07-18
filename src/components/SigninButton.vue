b6b3e82dae5849cfb95b3984f87d28973618e59b
geovanens <geovane.silva@ccc.ufcg.edu.br>
2018-07-18 01:55:50 -0300

    alterar tela de login, adicionar componentes Register, siginButton, notFound

---
+++ src/components/SigninButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="register" class="ui middle aligned center aligned grid">
+        <div ref="signinBtn" class="btn-sign-in"><img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" alt=""></div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'g-signin-button',
+    data () {
+        return {};
+    },
+    mounted () {
+        window.gapi.load('auth2', () => {
+        const auth2 = window.gapi.auth2.init({
+            client_id: '425992078532-o6usuafa3rganc10vkkae4oe4othp51j.apps.googleusercontent.com',
+            cookiepolicy: 'single_host_origin'
+        })
+        auth2.attachClickHandler(this.$refs.signinBtn, {}, googleUser => {
+            this.$emit('done', googleUser)
+        }, error => console.log(error))
+        })
+    },
+}
+</script>
+
+<style>
+.btn-sign-in {
+  cursor: pointer;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import Home from './components/Home.vue'
 import Navbar from './components/Navbar.vue'
 import Login from './components/Login.vue'
+import Register from './components/Register.vue'
+import notFound from './components/NotFound.vue'
 
 Vue.use(Router);
 
@@ -17,6 +19,16 @@ export default new Router({
             path: '/home',
             name: 'home',
             component: Home
-        }
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: Register
+        },
+        {
+            path: '/404',
+            name: 'notFound',
+            component: notFound
+        } 
     ]
 })
